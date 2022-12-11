5c105ba8ecd5b7eac8d67545d9faad2fc0e79e7b
Daniel Low <daniel@charmtx.com>
2022-12-11 13:22:12 +0000

    feat: parse monkey day 11

--- 2022/src/bin/day11.rs
+++ 2022/src/bin/day11.rs
@@ -9,7 +9,70 @@ struct Monkey {
     score: i64,
 }
 
+fn into_divisors(x: &i64) -> HashMap<i64, i64> {
+    let mut m = HashMap::new();
+    m.insert(2, *x % 2);
+    m.insert(3, *x % 3);
+    m.insert(5, *x % 5);
+    m.insert(7, *x % 7);
+    m.insert(11, *x % 11);
+    m.insert(13, *x % 13);
+    m.insert(17, *x % 17);
+    m.insert(19, *x % 19);
+    return m;
+}
+
 impl Monkey {
+    fn new(ss: &str) -> Self {
+        // Monkey 0:
+        //   Starting items: 74, 73, 57, 77, 74
+        //   Operation: new = old * 11
+        //   Test: divisible by 19
+        //     If true: throw to monkey 6
+        //     If false: throw to monkey 7
+
+        let lines: Vec<&str> = ss.split("\n").collect();
+        let parts = lines[1].split_once(":").unwrap();
+        let items: Vec<i64> = parts
+            .1
+            .trim()
+            .split(",")
+            .map(|x| x.trim().parse::<i64>().unwrap())
+            .collect();
+
+        let items2: Vec<HashMap<i64, i64>> = items.iter().map(into_divisors).collect();
+        let op_parts: Vec<&str> = lines[2].split(" ").collect();
+        let op_fn = op_parts[op_parts.len() - 2];
+        let op: Box<dyn Fn(i64) -> i64> = match (op_fn, op_parts[op_parts.len() - 1]) {
+            (x, "old") => match x {
+                "*" => Box::new(|x| x * x),
+                _ => panic!("Unknown operation"),
+            },
+            (x, y) => {
+                let y = y.parse::<i64>().unwrap();
+                let z: Box<dyn Fn(i64) -> i64> = match x {
+                    "+" => Box::new(move |a: i64| a + y),
+                    "*" => Box::new(move |a: i64| a * y),
+                    _ => panic!("Unknown operation"),
+                };
+                z
+            }
+        };
+        let get_last_number = |x: &str| {
+            return x.split(" ").last().unwrap().trim().parse::<i64>().unwrap();
+        };
+        let divisor = get_last_number(lines[3]);
+        let when_true = get_last_number(lines[4]) as usize;
+        let when_false = get_last_number(lines[5]) as usize;
+
+        Self {
+            items: items,
+            items2: items2,
+            test: (divisor, (when_true, when_false)),
+            op: op,
+            score: 0,
+        }
+    }
     fn do_turn2(&mut self) -> Vec<(HashMap<i64, i64>, usize)> {
         self.score += self.items2.len() as i64;
         let copy = self.items2.clone();
@@ -47,95 +110,18 @@ impl Monkey {
     }
 }
 
-fn hardcoded_monkeys() -> Vec<Monkey> {
-    let monkeys = vec![
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (19, (6, 7)),
-            op: Box::new(|x| x * 11),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (2, (6, 0)),
-            op: Box::new(|x| x + 8),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (3, (5, 3)),
-            op: Box::new(|x| x + 1),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (17, (5, 4)),
-            op: Box::new(|x| x * 7),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (13, (0, 1)),
-            op: Box::new(|x| x + 4),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (7, (1, 4)),
-            op: Box::new(|x| x + 7),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (5, (7, 2)),
-            op: Box::new(|x| x * x),
-            score: 0,
-        },
-        Monkey {
-            items: vec![],
-            items2: vec![],
-            test: (11, (2, 3)),
-            op: Box::new(|x| x + 6),
-            score: 0,
-        },
-    ];
-    return monkeys;
-}
-
 fn main() {
     let s: String = fs::read_to_string("./src/input11.txt").unwrap();
     // Only parse the items and hardcode the monkey instead
-    let ss: Vec<Vec<i64>> = s
-        .trim_end()
-        .split("\n\n")
-        .map(|x| {
-            let lines: Vec<&str> = x.split("\n").collect();
-            let parts = lines[1].split_once(":").unwrap();
-            return parts
-                .1
-                .trim()
-                .split(",")
-                .map(|x| x.trim().parse::<i64>().unwrap())
-                .collect();
-        })
-        .collect();
+    let mut ss: Vec<Monkey> = s.trim_end().split("\n\n").map(Monkey::new).collect();
 
-    part1(&ss);
-    part2(&ss);
+    part1(&mut ss);
+
+    let mut ss: Vec<Monkey> = s.trim_end().split("\n\n").map(Monkey::new).collect();
+    part2(&mut ss);
 }
 
-fn part1(ss: &Vec<Vec<i64>>) -> () {
-    let mut monkeys = hardcoded_monkeys();
-    for i in 0..ss.len() {
-        monkeys[i].items = ss[i].clone();
-    }
+fn part1(monkeys: &mut Vec<Monkey>) -> () {
     for _ in 0..20 {
         for i in 0..monkeys.len() {
             let active = &mut monkeys[i];
@@ -151,25 +137,7 @@ fn part1(ss: &Vec<Vec<i64>>) -> () {
     println!("{:?}", scores[0] * scores[1]);
 }
 
-fn part2(ss: &Vec<Vec<i64>>) -> () {
-    let mut monkeys = hardcoded_monkeys();
-    for i in 0..ss.len() {
-        monkeys[i].items2 = ss[i]
-            .iter()
-            .map(|x| {
-                let mut m = HashMap::new();
-                m.insert(2, *x % 2);
-                m.insert(3, *x % 3);
-                m.insert(5, *x % 5);
-                m.insert(7, *x % 7);
-                m.insert(11, *x % 11);
-                m.insert(13, *x % 13);
-                m.insert(17, *x % 17);
-                m.insert(19, *x % 19);
-                return m;
-            })
-            .collect();
-    }
+fn part2(monkeys: &mut Vec<Monkey>) -> () {
     for _ in 0..10000 {
         for i in 0..monkeys.len() {
             let active = &mut monkeys[i];
