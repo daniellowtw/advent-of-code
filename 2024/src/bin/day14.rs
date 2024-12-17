afb0032fb3bd49ca478d6b8fc55cb19670b39128
Daniel Low <daniel@charmtx.com>
2024-12-17 10:00:30 +0000

    day14: more generalization

--- 2024/src/bin/day14.rs
+++ 2024/src/bin/day14.rs
@@ -27,7 +27,60 @@ fn part1(pi: &Vec<(i32, i32, i32, i32)>, width: i32, height: i32) -> i32 {
     return counts.iter().fold(1, |acc, x| acc * x);
 }
 
+fn calculate_dist_sq(points: &Vec<(i32, i32)>) -> i64 {
+    let mut dist = 0;
+    for (px, py) in points.iter() {
+        for (qx, qy) in points.iter() {
+            dist += ((*px as i64 - *qx as i64).abs() + (*py as i64 - *qy as i64).abs()).pow(2);
+        }
+    }
+    return dist;
+}
+
 fn part2(pi: &Vec<(i32, i32, i32, i32)>, width: i32, height: i32) -> i32 {
+    // The following algo works after solving it the first time and understanding what the tree looks like.
+    // The assumption here is that the correct answer has the distance minimized.
+
+    // The idea here is we find a time where the distance between all the points is minimized.
+    // From 1 to 103, we know we would find x such that x == ans % 101, or x == ans % 103.
+    // We won't know which state we are in, but we find the minimum in both states.
+    let mut min_dist = 9e9 as i64;
+    let mut ans = 0;
+    for i in 1..=101 {
+        let mut points: Vec<(i32, i32)> = vec![];
+        for (x, y, z, w) in pi {
+            let nx = ((x + z * i) % width + width) % width;
+            let ny = ((y + w * i) % height + height) % height;
+            points.push((nx, ny));
+        }
+
+        let dist = calculate_dist_sq(&points);
+        if dist < min_dist {
+            min_dist = dist;
+            ans = i;
+        }
+    }
+    // So now we need to determine if we are in the 101 case or 103 case. So just try both.
+    for step in [101, 103].iter() {
+        for i in (ans..=101 * 103).step_by(*step) {
+            let mut points: Vec<(i32, i32)> = vec![];
+            for (x, y, z, w) in pi {
+                let nx = ((x + z * i) % width + width) % width;
+                let ny = ((y + w * i) % height + height) % height;
+                points.push((nx, ny));
+            }
+
+            let dist = calculate_dist_sq(&points);
+            if dist < min_dist {
+                min_dist = dist;
+                ans = i;
+            }
+        }
+    }
+    return ans;
+}
+
+fn _part2_original(pi: &Vec<(i32, i32, i32, i32)>, width: i32, height: i32) -> i32 {
     // for i in (1..=101*103).step_by(101) {
     // This is my first attempt. It is imperative to note that 101 and 103 are prime. So we know the x positions will repeat every 101 and y positions will repeat every 103. So the grid will repeat every 101*103.
     // I ran this and saved the output in a txt file. And noticed that in certain iterations, the entrophy is very low. There are some grids where the x axis distribution is concentrated, and some where y is concentrated.
